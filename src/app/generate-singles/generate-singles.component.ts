fad59826eb41cfa1f3c99cc9ab81b36952e1dd2e
lucerohospina <lucerojhp@gmail.com>
2018-07-02 14:37:23 -0500

    adding input method

--- src/app/generate-singles/generate-singles.component.ts
+++ src/app/generate-singles/generate-singles.component.ts
@@ -8,12 +8,33 @@ import { DataService } from '../services/data.service';
 export class GenerateSinglesComponent implements OnInit {
   hasValue = false;
   userName = '';
+  // data que va venir del form
+  name='';
+  dateToday='';
+  reference='';
+  placeIssue='';
+  quantity='';
+  amount='';
+  frecuency='';
+
   public dataGenerated = [];
 
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
     this.dataGenerated = this.dataService.getData();
+  }
+
+  // evento input para cada input element del form
+  generateTable(event:Event) {
+    // this.name = (<HTMLInputElement>event.target).value;
+    this.dateToday = (<HTMLInputElement>event.target).value;
+    // this.reference = (<HTMLInputElement>event.target).value;
+    // this.placeIssue = (<HTMLInputElement>event.target).value;
+    // this.quantity = (<HTMLInputElement>event.target).value;
+    // this.amount = (<HTMLInputElement>event.target).value;
+    // this.frecuency = (<HTMLInputElement>event.target).value;
+    console.log(this.dateToday);
   }
 
   resetingInput() {
